42b9c9859bfda813cc6b594bfc5cea6cc5f5b1ff
steamvinstudios <54758623+steamvinstudios@users.noreply.github.com>
2021-09-03 17:35:52 +0300

    Чистка

--- sampleSample/Program.cs
+++ sampleSample/Program.cs
@@ -1,20 +1,20 @@
-﻿// читаем путь
+﻿using System.Diagnostics;
 
-using System.Diagnostics;
-
-//var path = "C:\\Users\\Steam\\Downloads\\super.txt"; //Console.ReadLine();
 Console.Write("Введите путь к файлу: ");
 var path = Console.ReadLine();
-// создаем бенчмарк
+
 var stopWatch = new Stopwatch();
+
 stopWatch.Start();
-// создаем обхект с текстом из файла
+
 var textFile = File.ReadAllText(path);
-// создаем коллекцию со словами
-var words = textFile.Split("\n").ToList<string>();
-// создаем коллекцию триплетов
+
+var words = textFile
+    .Split("\n")
+    .ToList<string>();
+
 var triplets = new List<string>();
-// делим слова на триплеты и добавляем в коллекцию триплетов
+
 foreach (var word in words)
 {
     var i = 0;
@@ -33,7 +33,6 @@ foreach (var word in words)
 
 var groupedTriplets = triplets.GroupBy(x => x);
 
-// хранение объектов триплетов
 var tripletCollection = new List<TripletObject>();
 
 foreach (var k in groupedTriplets)
@@ -41,25 +40,12 @@ foreach (var k in groupedTriplets)
     tripletCollection.Add(new TripletObject { Name = k.Key, Rating = k.Count() });
 }
 
-foreach (var item in tripletCollection)
-{
-    Console.WriteLine($"{item.Name}: {item.Rating}");
-}
-
-// отсортируем коллекцию триплетов
 var sortedTripletCollection = tripletCollection
     .AsParallel()
     .OrderBy(x => x.Rating)
     .Reverse()
     .ToList();
 
-Console.WriteLine("Отсортированная коллекция триплетов:");
-foreach (var item in sortedTripletCollection)
-{
-    Console.WriteLine($"{item.Name}: {item.Rating}");
-}
-
-// выводим топ 10 через запятую
 Console.WriteLine("Топ 10");
 Console.ForegroundColor = ConsoleColor.Blue;
 for (int i = 0; i < 10; i++)
@@ -69,9 +55,12 @@ for (int i = 0; i < 10; i++)
 Console.ResetColor();
 
 stopWatch.Stop();
+
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine($"\nВыполнено за {stopWatch.ElapsedMilliseconds} мс");
 Console.ResetColor();
+
+Console.ReadLine();
 
 class TripletObject
 {
